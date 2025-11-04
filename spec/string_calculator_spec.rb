c888c032eeb6218b17ee970ea89d0c723a1bb63d
Ankit Nagar <ankitnagar@Ankit-Nagars-MacBook-M4-Pro.local>
2025-11-04 12:44:36 +0530

    Implement: support custom delimeter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe StringCalculator do
       expect(calc.add("1\n2,3")).to eq(6)
     end
 
-    it "supports custom single-character delimiter with header" do
+    it "handles custom  delimiter" do
       expect(calc.add("//;\n1;2")).to eq(3)
     end
   end
